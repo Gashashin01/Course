35d52bbaa1fca5f145c0499441f309010d75b05e
Gashashin01 <ferguspetr@gmail.com>
2021-11-17 18:13:05 +0200

    Copy function

---
+++ CopyTool/CopyTool/CopyTool.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <windows.h>
+
+bool Copy(const char* pathFrom, const char* pathTo) {
+    return CopyFileExA(pathFrom, pathTo, 0, 0, 0, COPY_FILE_FAIL_IF_EXISTS);
+}
+
+int main(int argc, char* argv[])
+{
+    bool copyState = Copy(argv[1], argv[2]);
+    if (copyState) {
+        std::cout << "Copied succesfully";
+    }
+    else {
+        std::cout << "Failed";
+    }
+    return 0;
+}
